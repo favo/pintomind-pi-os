d30049ea5eacb86aa93670934273a16c80a31443
Georg Skuggedal <georg@favo.no>
2024-05-15 09:34:23 +0200

    added prettier config file

--- PinToMindOS/src/modules/pintomindos/filesystem/home/pi/ble-bridge/app.js
+++ PinToMindOS/src/modules/pintomindos/filesystem/home/pi/ble-bridge/app.js
@@ -1,93 +1,91 @@
 import bleno from "bleno";
 import { Server } from "socket.io";
 import http from "http";
-import { configurationService, bleCallbacks, SERVICE_UUID} from "./ble-service.js";
+import { configurationService, bleCallbacks, SERVICE_UUID } from "./ble-service.js";
 const io = new Server();
-
 
 let blenoReady = false;
 let isSubscribed = false;
-let networkListCallback
-let networkConnectionCallback
+let networkListCallback;
+let networkConnectionCallback;
 
 bleno.on("stateChange", (state) => {
-	console.log("on -> stateChange: " + state);
-	if (state === "poweredOn") {
-		blenoReady = true;
-	}
+    console.log("on -> stateChange: " + state);
+    if (state === "poweredOn") {
+        blenoReady = true;
+    }
 });
 
 bleno.on("advertisingStart", (err) => {
-	if (err) {
-		console.log(err);
-		return;
-	}
-	console.log("advertising...");
-	bleno.setServices([configurationService]);
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log("advertising...");
+    bleno.setServices([configurationService]);
 });
 
 io.on("connection", (socket) => {
-	console.log("a user connected");
-	socket.on("ble-enable", (advertismentName) => {
-		console.log("ble-enable");
-		if (blenoReady) {
-			bleno.startAdvertising(advertismentName, [SERVICE_UUID]);
-			io.emit("ble-enabled");
-		}
-	});
+    console.log("a user connected");
+    socket.on("ble-enable", (advertismentName) => {
+        console.log("ble-enable");
+        if (blenoReady) {
+            bleno.startAdvertising(advertismentName, [SERVICE_UUID]);
+            io.emit("ble-enabled");
+        }
+    });
 
-	socket.on("ble-disable", () => {
-		bleno.stopAdvertising();
-		io.emit("ble-disabled");
-	});
+    socket.on("ble-disable", () => {
+        bleno.stopAdvertising();
+        io.emit("ble-disabled");
+    });
 
-	socket.on("network-connection-result", (result) => {
-		console.log("isSubscribed", isSubscribed);
-		console.log("network-connection-result", networkConnectionCallback);
-		if (isSubscribed && networkConnectionCallback != null) {
-			console.log("network-connection-result socket.on", result);
-			const buffer = new Buffer( result.toString() );
-			networkConnectionCallback(buffer)
-		}
-	})
+    socket.on("network-connection-result", (result) => {
+        console.log("isSubscribed", isSubscribed);
+        console.log("network-connection-result", networkConnectionCallback);
+        if (isSubscribed && networkConnectionCallback != null) {
+            console.log("network-connection-result socket.on", result);
+            const buffer = new Buffer(result.toString());
+            networkConnectionCallback(buffer);
+        }
+    });
 
-	socket.on("list-of-networks", (data) => {
-		console.log(data);
-		if (networkListCallback) {
-			const buffer = new Buffer( data.toString() );
-			networkListCallback(bleno.Characteristic.RESULT_SUCCESS, buffer);
-		}
-	})
+    socket.on("list-of-networks", (data) => {
+        console.log(data);
+        if (networkListCallback) {
+            const buffer = new Buffer(data.toString());
+            networkListCallback(bleno.Characteristic.RESULT_SUCCESS, buffer);
+        }
+    });
 });
 
 bleCallbacks.onSetRoatation = (rotation) => {
-	io.sockets.emit("rotation", rotation);
+    io.sockets.emit("rotation", rotation);
 };
 
 bleCallbacks.onSetWIFI = (data) => {
-	console.log(data);
-	io.sockets.emit("wifi", data);
+    console.log(data);
+    io.sockets.emit("wifi", data);
 };
 
 bleCallbacks.onSetHost = (host) => {
-	io.sockets.emit("host", host);
-}
+    io.sockets.emit("host", host);
+};
 
 bleCallbacks.sendNetworkList = (callback) => {
-	networkListCallback = callback
-	io.sockets.emit("get-network-list");
-}
+    networkListCallback = callback;
+    io.sockets.emit("get-network-list");
+};
 
 bleCallbacks.notifyNetworkConnection = (status, callback) => {
-	networkConnectionCallback = callback
-	isSubscribed = status
-}
+    networkConnectionCallback = callback;
+    isSubscribed = status;
+};
 
 bleCallbacks.finishSetup = () => {
-	io.sockets.emit("finish-setup");
-}
+    io.sockets.emit("finish-setup");
+};
 
 const httpServer = http.createServer();
 io.attach(httpServer);
 httpServer.listen(3333, "127.0.0.1");
-
--- PinToMindOS/src/modules/pintomindos/filesystem/home/pi/ble-bridge/ble-service.js
+++ PinToMindOS/src/modules/pintomindos/filesystem/home/pi/ble-bridge/ble-service.js
@@ -1,101 +1,94 @@
 import bleno from "bleno";
 
 // These can be any sequence of 32 hex digits
-export const SERVICE_UUID = '89496822200000000000000000000000';
-const ROTATION_CHARACTERISTIC_UUID = '89496822201000000000000000000000';
-const WIFI_CHARACTERISTIC_UUID = '89496822202000000000000000000000';
-const HOST_CHARACTERISTIC_UUID = '89496822204000000000000000000000';
-const FINISH_CHARACTERISTIC_UUID = '89496822209000000000000000000000';
+export const SERVICE_UUID = "89496822200000000000000000000000";
+const ROTATION_CHARACTERISTIC_UUID = "89496822201000000000000000000000";
+const WIFI_CHARACTERISTIC_UUID = "89496822202000000000000000000000";
+const HOST_CHARACTERISTIC_UUID = "89496822204000000000000000000000";
+const FINISH_CHARACTERISTIC_UUID = "89496822209000000000000000000000";
 
-const READ_NETWORK_LIST_CHARACTERISTIC_UUID = '89496822205000000000000000000000';
-const NOTIFY_NETWORK_CONNECTION_CHARACTERISTIC_UUID = '89496822206000000000000000000000';
+const READ_NETWORK_LIST_CHARACTERISTIC_UUID = "89496822205000000000000000000000";
+const NOTIFY_NETWORK_CONNECTION_CHARACTERISTIC_UUID = "89496822206000000000000000000000";
 
 export const bleCallbacks = {
-	onSetRoatation: (rotation) => {},
-	onSetWIFI: (ssid) => {},
-	onSetHost: (host) => {},
-	sendNetworkList: (callback) => {},
-	notifyNetworkConnection: (isSubscribed, callback) => {},
-	finishSetup: () => {},
+    onSetRoatation: (rotation) => {},
+    onSetWIFI: (ssid) => {},
+    onSetHost: (host) => {},
+    sendNetworkList: (callback) => {},
+    notifyNetworkConnection: (isSubscribed, callback) => {},
+    finishSetup: () => {},
 };
 
 const setRotationCharacteristic = new bleno.Characteristic({
-	uuid: ROTATION_CHARACTERISTIC_UUID,
-	properties: ['write'],
-	onWriteRequest: (data, _offset, _withoutResponse, callback) => {
-		const dataString = data.toString('utf-8');
-		console.log('setRotationCharacteristic write request: ' + data.toString('utf-8'));
-		if (['normal', 'left', 'right', 'inverted'].includes(dataString)) {
-			bleCallbacks.onSetRoatation(dataString);
-			callback(bleno.Characteristic.RESULT_SUCCESS);
-		} else {
-			callback(bleno.Characteristic.RESULT_UNLIKELY_ERROR);
-		}
-	},
+    uuid: ROTATION_CHARACTERISTIC_UUID,
+    properties: ["write"],
+    onWriteRequest: (data, _offset, _withoutResponse, callback) => {
+        const dataString = data.toString("utf-8");
+        console.log("setRotationCharacteristic write request: " + data.toString("utf-8"));
+        if (["normal", "left", "right", "inverted"].includes(dataString)) {
+            bleCallbacks.onSetRoatation(dataString);
+            callback(bleno.Characteristic.RESULT_SUCCESS);
+        } else {
+            callback(bleno.Characteristic.RESULT_UNLIKELY_ERROR);
+        }
+    },
 });
 
 const setWIFICharacteristic = new bleno.Characteristic({
-	uuid: WIFI_CHARACTERISTIC_UUID,
-	properties: ['write'],
-	onWriteRequest: (data, _offset, _withoutResponse, callback) => {
-		console.log('setWIFICharacteristic write request: ' + data.toString('utf-8'));
-		bleCallbacks.onSetWIFI(data.toString('utf-8'));
-		callback(bleno.Characteristic.RESULT_SUCCESS);
-	},
+    uuid: WIFI_CHARACTERISTIC_UUID,
+    properties: ["write"],
+    onWriteRequest: (data, _offset, _withoutResponse, callback) => {
+        console.log("setWIFICharacteristic write request: " + data.toString("utf-8"));
+        bleCallbacks.onSetWIFI(data.toString("utf-8"));
+        callback(bleno.Characteristic.RESULT_SUCCESS);
+    },
 });
 
 const setHostCharacteristic = new bleno.Characteristic({
-	uuid: HOST_CHARACTERISTIC_UUID,
-	properties: ['write'],
-	onWriteRequest: (data, _offset, _withoutResponse, callback) => {
-		console.log('setHostCharacteristic write request: ' + data.toString('utf-8'));
-		bleCallbacks.onSetHost(data.toString('utf-8'));
-		callback(bleno.Characteristic.RESULT_SUCCESS);
-	},
+    uuid: HOST_CHARACTERISTIC_UUID,
+    properties: ["write"],
+    onWriteRequest: (data, _offset, _withoutResponse, callback) => {
+        console.log("setHostCharacteristic write request: " + data.toString("utf-8"));
+        bleCallbacks.onSetHost(data.toString("utf-8"));
+        callback(bleno.Characteristic.RESULT_SUCCESS);
+    },
 });
 
 const readNetworkListCharacteristic = new bleno.Characteristic({
-	uuid: READ_NETWORK_LIST_CHARACTERISTIC_UUID,
-	properties: ['read'],
-	onReadRequest: (_offset, callback) => {
-		console.log('readNetworkListCharacteristic read request');
-		bleCallbacks.sendNetworkList(callback);
-	},
+    uuid: READ_NETWORK_LIST_CHARACTERISTIC_UUID,
+    properties: ["read"],
+    onReadRequest: (_offset, callback) => {
+        console.log("readNetworkListCharacteristic read request");
+        bleCallbacks.sendNetworkList(callback);
+    },
 });
 
 const notifyNetworkConnectionCharacteristic = new bleno.Characteristic({
-	uuid: NOTIFY_NETWORK_CONNECTION_CHARACTERISTIC_UUID,
-	properties: ['notify'],
-	onSubscribe: (maxValueSize, updateValueCallback) => {
-		console.log('notifyNetworkConnectionCharacteristic - onSubscribe');
-		bleCallbacks.notifyNetworkConnection(true, updateValueCallback);
-	},
-	onUnsubscribe: () => {
-		console.log('notifyNetworkConnectionCharacteristic - onUnsubscribe');
-		bleCallbacks.notifyNetworkConnection(false, null);
-	}
+    uuid: NOTIFY_NETWORK_CONNECTION_CHARACTERISTIC_UUID,
+    properties: ["notify"],
+    onSubscribe: (maxValueSize, updateValueCallback) => {
+        console.log("notifyNetworkConnectionCharacteristic - onSubscribe");
+        bleCallbacks.notifyNetworkConnection(true, updateValueCallback);
+    },
+    onUnsubscribe: () => {
+        console.log("notifyNetworkConnectionCharacteristic - onUnsubscribe");
+        bleCallbacks.notifyNetworkConnection(false, null);
+    },
 });
 
 const finishCharacteristic = new bleno.Characteristic({
-	uuid: FINISH_CHARACTERISTIC_UUID,
-	properties: ['write'],
-	onWriteRequest: (data, _offset, _withoutResponse, callback) => {
-		console.log('finishCharacteristic write request: ' + data.toString('utf-8'));
-		bleCallbacks.finishSetup();
-		callback(bleno.Characteristic.RESULT_SUCCESS);
-	},
+    uuid: FINISH_CHARACTERISTIC_UUID,
+    properties: ["write"],
+    onWriteRequest: (data, _offset, _withoutResponse, callback) => {
+        console.log("finishCharacteristic write request: " + data.toString("utf-8"));
+        bleCallbacks.finishSetup();
+        callback(bleno.Characteristic.RESULT_SUCCESS);
+    },
 });
 
 export const configurationService = new bleno.PrimaryService({
-	uuid: SERVICE_UUID,
-	characteristics: [
-		setRotationCharacteristic,
-		setWIFICharacteristic,
-		setHostCharacteristic,
-		readNetworkListCharacteristic,
-		notifyNetworkConnectionCharacteristic,
-		finishCharacteristic
-	]
+    uuid: SERVICE_UUID,
+    characteristics: [setRotationCharacteristic, setWIFICharacteristic, setHostCharacteristic, readNetworkListCharacteristic, notifyNetworkConnectionCharacteristic, finishCharacteristic],
 });
 
 // Endre firstpage. Så heller man kan gå til settings
